fbe65c6c1d4c8dd5c5beb6d43a4005d7b70f4cf5
Danil Dubrovksy <forwot163@gmail.com>
2018-11-21 22:25:23 +0300

    Quack fixes

--- src/lab6/Quack.java
+++ src/lab6/Quack.java
@@ -5,13 +5,13 @@ import java.util.*;
 
 public class Quack {
 
-    private static final int LIM = 65536;
+    private static final int LIM = 65535;
 
     Deque<Integer> q = new LinkedList<>();
     Map<String, Integer> registry = new HashMap<>();
     Map<String, Integer> labels = new HashMap<>();
 
-
+    private static PrintWriter out;
     public static void main(String[] args) throws IOException {
         BufferedReader reader = new BufferedReader(new FileReader(new File("quack.in")));
 
@@ -19,10 +19,13 @@ public class Quack {
         String inputLine;
         while ((inputLine = reader.readLine()) != null)
             input.add(inputLine);
+        out = new PrintWriter("quack.out");
 
         Quack quack = new Quack();
         quack.compile(input);
         quack.run(input);
+
+        out.close();
     }
 
     private void compile(List<String> input){
@@ -40,11 +43,11 @@ public class Quack {
     private int get(){
         if(q.isEmpty())
             return 0;
-        return q.removeFirst() % LIM;
+        return q.removeFirst() & LIM;
     }
 
     private void put(int x){
-        q.addLast(x % LIM);
+        q.addLast(x & LIM);
     }
 
     private void setReg(String reg, int x){
@@ -52,7 +55,7 @@ public class Quack {
     }
 
     private int getReg(String reg){
-        return registry.get(reg);
+        return registry.getOrDefault(reg, 0);
     }
 
     private void run(List<String> input){
@@ -77,21 +80,26 @@ public class Quack {
             } else if(line.startsWith("<")){
                 put(getReg(line.substring(1)));
             } else if(line.startsWith("P")){
-                System.out.println(line.length() > 1 ? getReg(line.substring(1)) : get());
+                out.println(line.length() > 1 ? getReg(line.substring(1)) : get());
             } else if(line.startsWith("C")){
                 int x = line.length() <= 1 ? get() : getReg(line.substring(1));
                 char c = (char) (x % 256);
-                System.out.print(c);
+                out.print(c);
             } else if(line.startsWith("J")){
                 i = labels.get(line.substring(1));
             } else if(line.startsWith("Z")){
                 int reg = getReg(String.valueOf(line.charAt(1)));
                 if(reg == 0)
                     i = labels.get(line.substring(2));
-            } else if(line.startsWith("G")){
+            } else if(line.startsWith("G")) {
                 int regFirst = getReg(String.valueOf(line.charAt(1)));
                 int regSecond = getReg(String.valueOf(line.charAt(2)));
-                if(regFirst > regSecond)
+                if (regFirst > regSecond)
+                    i = labels.get(line.substring(3));
+            } else if(line.startsWith("E")){
+                int regFirst = getReg(String.valueOf(line.charAt(1)));
+                int regSecond = getReg(String.valueOf(line.charAt(2)));
+                if(regFirst == regSecond)
                     i = labels.get(line.substring(3));
             } else if("Q".equals(line)){
                 break;
